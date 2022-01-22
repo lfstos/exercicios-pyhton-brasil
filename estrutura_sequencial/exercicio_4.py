c65c2730bfd174baddf2c6d8332a95c08856b74c
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-01-22 15:19:28 -0300

    Média de 4 valores informado pelo usuário

---
+++ estrutura_sequencial/exercicio_4.py
@@ -0,0 +1,8 @@
+num_1 = float(input('Digite a 1ª nota: '))
+num_2 = float(input('Digite a 2º nota: '))
+num_3 = float(input('Digite a 3ª nota: '))
+num_4 = float(input('Digite a 4ª nota: '))
+
+media = (num_1 + num_2 + num_3 + num_4) / 4
+
+print('A média de {0} + {1} + {2}+ {3} = {4}'.format(num_1, num_2, num_3, num_4, media))

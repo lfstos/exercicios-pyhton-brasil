5bd793ee1994df2509ae80855a729a02d902460a
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-01-22 17:23:47 -0300

    Mostra salário com base em horas trabalhadas por mês e valor ganho por hora

---
+++ estrutura_sequencial/exercicio_8.py
@@ -0,0 +1,7 @@
+
+valor_hora = float(input('Quanto ganha por hora: '))
+horas_trabalhada = float(input('Quantas hora vc trabalha por mês: '))
+
+salario = horas_trabalhada * valor_hora
+
+print('Seu salário é {0}'.format(salario))

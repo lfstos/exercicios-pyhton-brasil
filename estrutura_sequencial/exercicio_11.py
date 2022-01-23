9a79a34718ca1dafca69543cae66cee877d22abc
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-01-22 21:37:46 -0300

    :rocek: Vários resultados

---
+++ estrutura_sequencial/exercicio_11.py
@@ -0,0 +1,11 @@
+num_1 = int(input('Digite o 1º inteiro: '))
+num_2 = int(input('Digite o 2º inteiro: '))
+num_3 = float(input('Digite o valor real: '))
+
+resultado_1 = (num_1 * 2) * (num_2 / 2)
+resultado_2 = (num_1 * 3) + num_3
+resultado_3 = num_3 ** 2
+
+print(f'O produto do dobro do primeiro com a metade do segundo é {resultado_1}')
+print(f'A soma do triplo do primeiro com o terceiro {resultado_2}')
+print(f'O terceiro elevado ao cubo é {resultado_3}')

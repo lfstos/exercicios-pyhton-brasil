3b0e7994f8789d55038acd9b88df1ca03bdd6492
Luiz Fernando <fernando@mixtelatacado.com.br>
2022-02-28 10:25:31 -0300

    :rocket: Criada função maior_numero

--- estrutura_decisao/exercicio_1.py
+++ estrutura_decisao/exercicio_1.py
@@ -1,7 +1,12 @@
-valor_1 = int(input('Digite um valor: '))
-valor_2 = int(input('Digite outro valor: '))
+def maior_numero():
+    valor_1 = int(input('Digite um valor: '))
+    valor_2 = int(input('Digite outro valor: '))
 
-if valor_1 > valor_2:
-    print(valor_1)
-else:
-    print(valor_2)
+    if valor_1 > valor_2:
+        return valor_1
+    else:
+        return valor_2
+
+if '__main__' ==__name__:
+    maior = maior_numero()
+    print(maior)
